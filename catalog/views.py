5f5b27615cd2db4305f28f4c8ba5878111fa1a51
Tsvetkova Nina <tsvnishka@gmail.com>
2023-05-10 23:40:27 +0300

    HW_Getting_Know_Django

--- catalog/views.py
+++ catalog/views.py
@@ -7,4 +7,11 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        print(f'\nuser: {name} \nphone: {phone} \nsend message: {message}\n')
+
     return render(request, "catalog/contacts.html")
