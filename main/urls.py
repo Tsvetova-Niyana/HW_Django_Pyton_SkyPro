1b5bf794c7cc7aa7e3d84cdc550f67b5dbed1cbf
Tsvetkova Nina <tsvnishka@gmail.com>
2023-05-15 20:46:10 +0300

    HW_DB_Django

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import index
+from main.views import index, contacts, students, student
 
 app_name = MainConfig.name
 
 urlpatterns = [
-    path("", index)
+    path('', index, name='index'),
+    path('contacts/', contacts, name='contacts'),
+    path('students/', students, name='students_list'),
+    path('student/<int:pk>/', student, name='student_item')
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,39 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+from main.models import Student
+
+
 def index(request):
+    context = {
+        'objects_list': Student.objects.all()[:3],
+        'title': 'Главная'
+    }
+
+    return render(request, "main/index.html", context)
+
+
+def students(request):
+    context = {
+        'objects_list': Student.objects.all(),
+        'title': 'Список студентов'
+    }
+
+    return render(request, "main/students.html", context)
+
+
+def student(request, pk):
+    student_item = Student.objects.get(pk=pk)
+
+    context = {
+        'object': student_item,
+        'title': student_item
+    }
+
+    return render(request, 'main/student.html', context)
+
+
+def contacts(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('email')
@@ -12,5 +43,8 @@ def index(request):
             f'user {name} with email {email} send message: {message}'
         )
 
-    return render(request, "main/index.html")
-    # pass
+    context = {
+        'title': 'Контакты'
+    }
+
+    return render(request, "main/contact.html", context)
